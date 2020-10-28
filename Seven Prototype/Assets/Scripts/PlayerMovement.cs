6f546c042dd1c812f7c22c18ccf16a544d2be1e9
ThomasApplewhite <44822750+ThomasApplewhite@users.noreply.github.com>
2020-10-28 15:30:57 -0700

    Movement fixed up
    Not quite the way I want it (since we call a move function every fixed update), but it works! The player can move and attacking is bound.

--- Seven Prototype/Assets/Scripts/PlayerMovement.cs
+++ Seven Prototype/Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+//using UnityEngine.InputSystem.CallbackContext;
 
 //I don't know what the actual name of the component is for the require lol
 [RequireComponent(typeof(CharacterController),typeof(PlayerInput))]
@@ -10,15 +11,15 @@ public class PlayerMovement : MonoBehaviour
     public float speedScalar = 5.0f;
 
     CharacterController controller;
-    InputAction movementAction;
+    Vector2 movementDirection;
 
     // Start is called before the first frame update
     void Start()
     {
         controller = this.gameObject.GetComponent<CharacterController>();
+        movementDirection = Vector2.zero;
         //having to keep a reference to the action being referenced defeats the purpose
         //of using Send Messages, but this seems to be the only way to do this right now
-        movementAction = this.gameObject.GetComponent<PlayerInput>().actions["Movement"];
         //Debug.Log(Keyboard.keyboardLayout);
     }
 
@@ -28,29 +29,40 @@ public class PlayerMovement : MonoBehaviour
 
     }
 
+    void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
     void OnMovement(InputValue input){
         //controller.Move(input.Get<Vector2>() * speedScalar * Time.deltaTime);
         //Debug.Log(input.isPressed);
-        StartCoroutine(DoMovement(input));
+        //StartCoroutine(DoMovement(input));
+        movementDirection = input.Get<Vector2>();
     }
 
-    IEnumerator DoMovement(InputValue input)
+    void MovePlayer()
+    {
+        controller.Move(movementDirection * speedScalar * Time.deltaTime);
+    }
+
+    /*IEnumerator DoMovement(InputAction.CallbackContext input)
     {
         Debug.Log("movement has been registered");
 
-        Vector2 movementInput = input.Get<Vector2>();
+        Vector2 movementInput = input.ReadValue<Vector2>();
         //InputAction inputAction = input.Get<InputAction>();
-        Debug.Log("Input retreieved");
+        Debug.Log("Input retreieved. Is button pressed? " + input.action.isPressed);
 
         //we scale by 1: speed and 2: time, to normalize the speed with the current flow of Unity Time  
-        /*while(movementAction.triggered)
+        /*while(input.isPressed)
         {
             Debug.Log("smoovin...");
             controller.Move(movementInput * speedScalar * Time.deltaTime);
-        }*/
+        }*
 
         yield return null;
-    }
+    }*/
 
     void OnAttack(){
         Debug.Log("attack has been registered");
