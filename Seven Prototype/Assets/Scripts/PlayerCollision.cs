f047b8fb1521b740e890d221cd7857f666727563
Mingun Cho <mingun0108@gmail.com>
2020-11-01 21:49:06 -0800

    deleted unnecssary script file

---
+++ Seven Prototype/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public string sceneToLoad;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("SceneChange"))
+        {
+            Debug.Log("Scene Changed to BossScene");
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
