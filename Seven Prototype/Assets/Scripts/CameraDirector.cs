c1572083de2b2ba567f9ae357df9c48b46103f49
Mingun Cho <mingun0108@gmail.com>
2020-11-01 18:06:58 -0800

    Implemented Camera Movement and Scene Transition

--- Seven Prototype/Assets/Scripts/CameraDirector.cs
+++ Seven Prototype/Assets/Scripts/CameraDirector.cs
@@ -5,25 +5,25 @@ using UnityEngine;
 [RequireComponent(typeof(Camera), typeof(Transform))]
 public class CameraDirector : MonoBehaviour
 {
-    public Transform originalTarget;
-    private Transform localTransform;
+    public Transform player;
+    //private Transform localTransform;
 
     // Start is called before the first frame update
     void Start()
     {
-        localTransform = this.gameObject.transform;
-        localTransform.SetParent(originalTarget);
+        //localTransform = this.gameObject.transform;
+        //localTransform.SetParent(originalTarget);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
     }
 
-    public void SwitchTarget(Transform newTarget){
+    /*public void SwitchTarget(Transform newTarget){
         localTransform.SetParent(newTarget);
-    }
+    }*/
 
     //one day I'm going to add methods to switch primaryTarget with methods
     //and maybe a "slide to here, look at it, then go back" method.
