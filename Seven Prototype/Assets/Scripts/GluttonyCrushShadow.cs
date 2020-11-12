5726e0baba5044fad0e5540b18d90e52855a9d39
Mingun Cho <mingun0108@gmail.com>
2020-11-11 17:38:49 -0800

    Implemented Gluttony's crush ability with better visual

---
+++ Seven Prototype/Assets/Scripts/GluttonyCrushShadow.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GluttonyCrushShadow : MonoBehaviour
+{
+    GameObject player;
+    GameObject gluttony;
+    bool freezePos = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        gluttony = GameObject.Find("Gluttony");
+        StartCoroutine(freezePosition());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!freezePos)
+        {
+            transform.position = new Vector2(player.transform.position.x, player.transform.position.y + 5.0f);
+            gluttony.transform.position = new Vector2(player.transform.position.x, this.gameObject.transform.position.y + 120.0f);
+        }
+        
+    }
+
+    IEnumerator freezePosition()
+    {
+        yield return new WaitForSeconds(2);
+        freezePos = true;
+        StartCoroutine(DestorySelf());
+    }
+
+    IEnumerator DestorySelf()
+    {
+        yield return new WaitForSeconds(1);
+        this.gameObject.SetActive(false);
+    }
+}
