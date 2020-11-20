6d9e8be87cbfca07c3b0fd181ebcc916c8be1cdb
ThomasApplewhite <44822750+ThomasApplewhite@users.noreply.github.com>
2020-11-19 21:13:36 -0800

    Implemented ActorAbility Abstract Class
    Abstract base class that all ActorAbilities can inherit from to ensure they have the correct functionality. It's not an interface like originally documented, but strict functionality rules are needed, and only an abstract class can provide them.

---
+++ Seven Prototype/Assets/Scripts/ActorAbility.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*Because this is a full class, it's the only thing Abilities
+can inherit from. I've made it a MonoBehaviour to make */
+public abstract class ActorAbility : MonoBehaviour
+{
+    public float cooldownPeriod = 5.0f;
+
+    private bool usable = true;
+
+    public bool getUsable(){return usable;}
+
+    //overridable coroutine for handling ability cooldowns.
+    public virtual IEnumerator coolDown(float period)
+    {
+        usable = false;
+        yield return new WaitForSeconds(period);
+        usable = true;
+    }
+
+    /*overridable method for activating the ability. While nothing is
+    making you call InternInvoke or start coolDown, you should still do
+    those manually.
+    
+    and, fyi, Invoke and InternInvoke are seperated so things using abilities
+    don't need to worry about conflicting argument and return types*/
+    public virtual void Invoke()
+    {
+        if(usable)
+        {
+            InternInvoke<int, int>(new int[0]);
+            StartCoroutine(coolDown(cooldownPeriod));
+        }
+        
+    }
+
+    /*this is only accessable to derivative classes, as it's protected
+    it's also the method that does the ability, feel free to make the
+    types whatever you want*/
+    protected abstract T2 InternInvoke<T1, T2>(params T1[] args);
+}
